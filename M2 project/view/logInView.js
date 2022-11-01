679761ba43dae73db6f03e0eb5696691a0f94c4e
UssCringe <david.petur@gmail.com>
2022-11-01 14:41:34 +0100

    Update logInView.js

--- M2 project/view/logInView.js
+++ M2 project/view/logInView.js
@@ -7,15 +7,46 @@
 //     app.innerHTML = html
 // }
 
-// logInView();
+logInView();
 function logInView() {
+  model.app.view ="login-menu"
+  view = model.app.view
+
   let app = document.getElementById("app");
-  console.log(model);
+  console.log(model)
+
   let html = "";
-  html += `
-    <img src="" alt="tennis icon"/>
-    <button>log in</button>
-    <button>register</button>
+  html +=  `
+  <img class="logo" src="assets/table-tennis-paddle-ball-solid.svg">
+    
+    <div class="login--inputs">${view === "login-menu"? "" : view ==="login"? `
+
+    <div><input placeholder="brukernavn" type="text"></div>
+    <div><input placeholder="passord" type="text"></div>`
+    : `
+    <div>
+      <input placeholder="førstenavn" type="text">
+      <input placeholder="etternavn" type="text">
+    </div>
+
+   
+      <input placeholder="brukernavn" type="text">
+   
+
+  
+      <input placeholder="passord" type="text">
+  
+   
+      <input placeholder="bekreft passord" type="text">
+   
+    
+
+    `}</div>
+
+   
+
+    <button>${view === "register" ? "submit" : "log in"}</button>
+    <button> ${view === "login-menu" ? "registrer" : "avbryt"}</button>
     `;
   app.innerHTML = html;
 }
