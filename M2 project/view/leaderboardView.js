023393ce375b86b137c6e4380ed9cd18f3df9923
UssCringe <david.petur@gmail.com>
2022-11-02 13:33:53 +0100

    begynte på leaderboardView

--- M2 project/view/leaderboardView.js
+++ M2 project/view/leaderboardView.js
@@ -1,1 +1,19 @@
-function leaderboardView() {}
+leaderboardView()
+function leaderboardView() {
+    let app = document.getElementById("app");
+    let html = ""
+    html += `
+    <img class="logo" src="assets/table-tennis-paddle-ball-solid.svg">
+    <h1>
+        <span>Tournaments</span>
+        <span>matches</span>
+    </h2>
+
+    <label for="choose">sort by</label>
+    <select name="choose">
+        <option value="wins">wins</option>
+        <option value="win rate">win rate</option>
+    </select>
+    `
+    app.innerHTML = html
+}
