e017505976aed1f46a85548cb4e8b688ab2f8d2f
Robert Lysebo <robertlysebo@gmail.com>
2022-11-14 15:40:10 +0100

    added button

--- M2 project/view/leaderboardView.js
+++ M2 project/view/leaderboardView.js
@@ -28,11 +28,10 @@ function leaderboardView() {
     </select>
 
     ${getWins()}
+    <button class="btn" onclick="menuView()">tilbake</button>
     `;
   app.innerHTML = html;
 }
-
-
 
 function getOptions() {
   let selectedOptionIndex = model.app.leaderboradSelectIndex;
@@ -48,10 +47,8 @@ function getOptions() {
   return html;
 }
 
-
-
 function getWins(winType) {
-  let wins = getStats()
+  let wins = getStats();
 
   html = `
     <table style="width:100%">
@@ -67,139 +64,133 @@ function getWins(winType) {
   return html;
 }
 
-
-
 function getStats() {
-  resetLastMatches()
-  timedMatches()
-  let category = model.inputs.leaderboard.category
-  let sortBy = model.inputs.leaderboard.sortBy
-  let users = model.data.users
+  resetLastMatches();
+  timedMatches();
+  let category = model.inputs.leaderboard.category;
+  let sortBy = model.inputs.leaderboard.sortBy;
+  let users = model.data.users;
 
-  let result = "" 
+  let result = "";
 
-  let winners = users
+  let winners = users;
   // Getting tournaments
 
-
   if (sortBy === "wins") {
-    winners = winners.sort((a, b) => category == "tournaments"
-      ? (a.lastTourneyWins < b.lastTourneyWins) || a.lastTourneyLosses + a.lastTourneyWins === 0 ? 1 : -1
-      : a.lastWins < b.lastWins || a.lastLosses + a.lastWins === 0 ? 1 : -1)
+    winners = winners.sort((a, b) =>
+      category == "tournaments"
+        ? a.lastTourneyWins < b.lastTourneyWins || a.lastTourneyLosses + a.lastTourneyWins === 0
+          ? 1
+          : -1
+        : a.lastWins < b.lastWins || a.lastLosses + a.lastWins === 0
+        ? 1
+        : -1
+    );
+  } else {
+    winners = users.sort((a, b) =>
+      category == "tournaments"
+        ? a.lastTourneyWins / (a.lastTourneyWins + a.lastTourneyLosses) <
+            b.lastTourneyWins / (b.lastTourneyWins + b.lastTourneyLosses) ||
+          a.lastTourneyLosses + a.lastTourneyWins === 0
+          ? 1
+          : -1
+        : a.lastWins / (a.lastWins + a.lastLosses) < b.lastWins / (b.lastWins + b.lastLosses) ||
+          a.lastLosses + a.lastWins === 0
+        ? 1
+        : -1
+    );
   }
 
-  else {
-    winners = users.sort((a, b) => category == "tournaments"
-      ? a.lastTourneyWins / (a.lastTourneyWins + a.lastTourneyLosses) <
-        b.lastTourneyWins / (b.lastTourneyWins + b.lastTourneyLosses) ||
-        a.lastTourneyLosses + a.lastTourneyWins === 0 ? 1 : -1
-      : a.lastWins / (a.lastWins + a.lastLosses) < b.lastWins / (b.lastWins + b.lastLosses) || a.lastLosses + a.lastWins === 0 ? 1 : -1)
-  }
-
-
-  result = winners.map((user, index) => category == "tournaments"
-    ? `
+  result = winners.map((user, index) =>
+    category == "tournaments"
+      ? `
       <tr>
         <th>${index + 1}</th>
         <th>${user.lastTourneyWins}/${user.lastTourneyLosses}</th>
-        <th>${user.lastTourneyWins + user.lastTourneyLosses === 0 ? "N/A" :
-      Math.round(user.lastTourneyWins / (user.lastTourneyWins + user.lastTourneyLosses) * 100) + "%"}
+        <th>${
+          user.lastTourneyWins + user.lastTourneyLosses === 0
+            ? "N/A"
+            : Math.round((user.lastTourneyWins / (user.lastTourneyWins + user.lastTourneyLosses)) * 100) + "%"
+        }
         </th>
         <th>${user.userName}</th>
       </tr>`
-    : `
+      : `
         <tr>
           <th>${index + 1}</th>
           <th>${user.lastWins}/${user.lastLosses}</th>
-          <th>${user.lastWins + user.lastLosses === 0 ? "N/A" :
-      Math.round((user.lastWins / (user.lastWins + user.lastLosses)) * 100) + "%"}
+          <th>${
+            user.lastWins + user.lastLosses === 0
+              ? "N/A"
+              : Math.round((user.lastWins / (user.lastWins + user.lastLosses)) * 100) + "%"
+          }
           </th>
           <th>${user.userName}</th>
-        </tr>`)
-  
-return result
+        </tr>`
+  );
+
+  return result;
 }
 
-
-
-
 function timedMatches() {
-  let matches = getDays("matches")
-  let tourneys = getDays("tournaments")
-  const users = model.data.users
+  let matches = getDays("matches");
+  let tourneys = getDays("tournaments");
+  const users = model.data.users;
 
   for (let match of matches) {
-
     for (let user of users) {
-      if ((match.participants[0].matchScore === 10 && match.participants[0].playerId === user.id)
-        || (match.participants[1].matchScore === 10 && match.participants[1].playerId === user.id)) {
-        user.lastWins += 1
-      }
-      else if ((match.participants[0].matchScore !== 10 && match.participants[0].playerId === user.id)
-        || (match.participants[1].matchScore !== 10 && match.participants[1].playerId === user.id)) {
-        user.lastLosses += 1
+      if (
+        (match.participants[0].matchScore === 10 && match.participants[0].playerId === user.id) ||
+        (match.participants[1].matchScore === 10 && match.participants[1].playerId === user.id)
+      ) {
+        user.lastWins += 1;
+      } else if (
+        (match.participants[0].matchScore !== 10 && match.participants[0].playerId === user.id) ||
+        (match.participants[1].matchScore !== 10 && match.participants[1].playerId === user.id)
+      ) {
+        user.lastLosses += 1;
       }
     }
   }
 
   for (let tourney of tourneys) {
-
     for (let user of users) {
       if (user.id === tourney.winnerId) {
-        user.lastTourneyWins += 1
-        continue
+        user.lastTourneyWins += 1;
+        continue;
       }
-      if (tourney.players.includes(user.id)) { user.lastTourneyLosses += 1 }
+      if (tourney.players.includes(user.id)) {
+        user.lastTourneyLosses += 1;
+      }
     }
   }
-
-
 }
 
 function resetLastMatches() {
-  let users = model.data.users
+  let users = model.data.users;
   for (let user of users) {
-    user.lastWins = 0
-    user.lastLosses = 0
-    user.lastTourneyWins = 0
-    user.lastTourneyLosses = 0
+    user.lastWins = 0;
+    user.lastLosses = 0;
+    user.lastTourneyWins = 0;
+    user.lastTourneyLosses = 0;
   }
 }
 
 function getDays(type) {
-  let matches = model.data.matches
-  let tourneys = model.data.tournaments
-  let time = model.inputs.leaderboard.showLast
+  let matches = model.data.matches;
+  let tourneys = model.data.tournaments;
+  let time = model.inputs.leaderboard.showLast;
 
-  if (!time && type == "matches") { return matches }
-  else if (!time && type == "tournaments") { return tourneys }
+  if (!time && type == "matches") {
+    return matches;
+  } else if (!time && type == "tournaments") {
+    return tourneys;
+  }
 
-  let selectedDays = model.inputs.leaderboard.showLast
+  let selectedDays = model.inputs.leaderboard.showLast;
   let daysInMs = selectedDays * 60 * 60 * 24 * 1000;
   let now = new Date().getTime();
 
   if (type == "matches") return matches.filter((match) => now - new Date(match.datePlayed).getTime() < daysInMs);
   else return tourneys.filter((tourney) => now - new Date(tourney.datePlayed).getTime() < daysInMs);
-
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
