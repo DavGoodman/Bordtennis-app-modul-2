2824f4ef53bd6c68efbafe60e34568c930e10142
Robert Lysebo <robertlysebo@gmail.com>
2022-11-16 19:17:18 +0100

    første del av brakett

--- M2 project/view/ongoingTourView.js
+++ M2 project/view/ongoingTourView.js
@@ -1,21 +1,99 @@
-let participants = ["Robhimself", "dankert", "Newbie", "Gjesten", "developer"];
+let participantsFive = ["Robhimself", "dankert", "Newbie", "Gjesten", "developer"]; // testarray for "ikke-gode" antall deltakere
+let participantsFour = ["Robhimself", "dankert", "Newbie", "developer"]; // testarray for "gode" antall deltakere
+model.data.ongoingTournament.tournamentName = "TorsdagsTourny m/ Terje!"; //  sample for view
+model.app.view = "round-1";
+
+//  alt over burde være satt når man lager turnering i tournamentView()
+
 ongoingTournamentView();
 function ongoingTournamentView() {
   let app = document.getElementById("app");
   let html = "";
+  html += /*html*/ `
+  <h3 class="rnd-title">
+    ${model.data.ongoingTournament.tournamentName}
+  </h3>
 
-  html += `
-    <div style="border: 2px solid black">Startskudd</div>
-    `;
-
+  <div class="rnd-num">
+    Runde ${tRound()}
+  </div>
+  <br>
+  <div>
+    ${checkBaseBracket() ? newBasicTournament() : getWeirdTournamentNumbers()}  
+  </div>
+  <br>
+  <button onclick="">
+    Neste Runde
+  </button>
+  `;
   app.innerHTML = html;
 }
 
-function checkBaseBracket(n) {
+function checkBaseBracket() {
   let base = [4, 8, 16, 32, 64, 128];
-  if (base.includes(n)) {
-  }
+  let n = participantsFour.length;
+
+  return base.includes(n);
 }
+
+function tRound() {
+  let str = model.app.view;
+  return str.charAt(str.length - 1);
+}
+
+function newBasicTournament() {
+  let list = participantsFour;
+  let matchNum = list.length / 2;
+  let roundArray = [];
+  let matchHtml = "";
+
+  if (roundArray.length < matchNum) {
+    for (let i = 0; i < list.length; i = i + 2) {
+      let matchArray = [];
+      matchArray.push(list[i]);
+      matchArray.push(list[i + 1]);
+      roundArray.push(matchArray);
+    }
+  }
+  for (let i = 0; i < roundArray.length; i++) {
+    matchHtml += `<div class="bracket-list">`; // start på en ramme rundt hver kamp.
+    for (let j = 0; j < roundArray[i].length; j++) {
+      matchHtml += `
+      <div class="rnd-mtch">
+        <div class="tm-name">
+          ${roundArray[i][j]}
+        </div>
+
+        <div class="tm-score">
+          <input class="score-input" type="number" maxlength="2" onchange=""><button onclick="alert('Er dette virkelig ${roundArray[i][j]}?')">Save</button>
+        </div>
+      </div>  
+      `;
+    }
+    matchHtml += `</div><br>`; //  stopp på ramme.
+  }
+
+  return matchHtml;
+}
+
+function getWeirdTournamentNumbers() {
+  let baseN = [128, 64, 32, 16, 8, 4];
+  let p = participantsFive.length;
+  let diff;
+  let goldenN;
+
+  for (let i = 0; i < baseN.length; i++) {
+    if (n < baseN[i] && n > baseN[i + 1]) {
+      diff = n - baseN[i + 1];
+      goldenN = baseN[i + 1];
+      break;
+    }
+  }
+  console.log("Woopsie, weird bracket! ", "Byes= ", diff, "Closest golden number = ", goldenN);
+  makeByersBracket(diff, goldenN);
+}
+
+function makeByersBracket(byers, closestToBase) {}
 
 // deltakere allerede stokket
 function listMatchups() {}
@@ -81,7 +159,7 @@ function generateRound(array) {
 //   if (n / 2 < 1) {
 //     return [];
 //   } else {
-//     if (n % 2 != 0){
+//     if (n % 2 != 0) {
 //       n++;
 //     }
 //     count = findRounds(n / 2);
