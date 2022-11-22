ad31fbf2ceba6b3e21ddc06a2533b9ab013df102
UssCringe <david.petur@gmail.com>
2022-11-22 09:59:29 +0100

    Update ongoingTourView.js

--- M2 project/view/ongoingTourView.js
+++ M2 project/view/ongoingTourView.js
@@ -135,5 +135,9 @@ function generateRound(array) {
 function winnerView(name) {
   document.getElementById(
     "app"
-  ).innerHTML = `<h1>${name} vant ${model.data.ongoingTournament.tournamentName}-turneringen! Den gærningen...</h1>`;
+  ).innerHTML = `
+  <h1>
+    ${name} vant ${model.data.ongoingTournament.tournamentName}-turneringen! Den gærningen...
+  </h1>
+  <button onclick="menuView()" class="btn filled">Tilbake</button>`;
 }
