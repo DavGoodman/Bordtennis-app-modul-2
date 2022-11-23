efc40ce24fc655383f5c0de746af5e2c1554ce5f
Simen Kristiansen <simenbolstad@gmail.com>
2022-11-23 10:31:14 +0100

    Update tournamentExperimental.js

--- tournamentExperimental.js
+++ tournamentExperimental.js
@@ -1,17 +1,19 @@
-const participants = ['anders', 'berit', 'clara', 'david', 'erlend', 'fredrik']
+const participants = ['anders', 'berit', 'clara', 'david', 'erlend', 'fredrik', 'geir', 'halvor', 'ivar', 'jonas', 'kurt', 'lars']
 let roundPlayers;
 
 function getPlayersAndByes(n) {
-    const base = [2, 4, 8, 16, 32, 64, 128];
+    let base = [2, 4];
     for (let i = 0; i < base.length; i++) {
         if (base[i] < n && n < base[i + 1]) {
             roundPlayers = (n - base[i]) * 2;
-
             const byeList = participants.filter((item, index) => index >= roundPlayers);
             const roundPlayerList = participants.filter((item, index) => index < roundPlayers)
+            console.log(roundPlayerList, byeList)
             return byeList, roundPlayerList
+        } else if (n > base[base.length - 1]) {
+            base.push(base[base.length - 1] * 2)
+            console.log('hei', base)
         }
     }
 }
-
-getPlayersAndByes(6)
+getPlayersAndByes(participants.length)
