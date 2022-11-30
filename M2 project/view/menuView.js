a06ecd6a9faa22c3512216fd38394cc01e2f905e
Robert Lysebo <robertlysebo@gmail.com>
2022-11-30 13:46:29 +0100

    update

--- M2 project/view/menuView.js
+++ M2 project/view/menuView.js
@@ -4,7 +4,7 @@ function menuView() {
   let app = document.getElementById("app");
   let html = "";
   html += `
-  <div class="menu-container">
+  <div class="top-container">
     <div class="logout-div" onclick="toLogin('login')">  
       <i class="fa-solid fa-arrow-right-from-bracket log-out-btn"></i>
     </div>    
@@ -12,15 +12,12 @@ function menuView() {
       <img class='logo' src="assets/Logo.svg" alt="tennis icon"/>
     </div>
   </div>
- 
-  
-        
-        <div class="button-container" style="height: 40vh">
-          <button onclick="matchView();" class="btn filled">SPILL</button>
-          <button onclick="tournamentView()" class="btn ">TURNERING</button>
-          <button onclick="leaderboardView()" class="btn ">LEDERTAVLE</button>
-          <button onclick="historyView()" class="btn ">HISTORIKK</button>
-        </div> 
+  <div class="button-container" style="height: 40vh">
+    <button onclick="matchView();" class="btn filled">SPILL</button>
+    <button onclick="tournamentView()" class="btn ">TURNERING</button>
+    <button onclick="leaderboardView()" class="btn ">LEDERTAVLE</button>
+    <button onclick="historyView()" class="btn ">HISTORIKK</button>
+  </div> 
         `;
   app.innerHTML = html;
 }
